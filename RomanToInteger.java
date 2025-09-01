5f9227f4db4acdbb8971ab0953ae27544541daf7
eduard.nitapopa@gmail.com <nitaedi@Nitas-MacBook-Air.local>
2025-09-01 16:32:43 +0300

    solution

---
+++ RomanToInteger.java
@@ -0,0 +1,28 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class RomanToInteger {
+    public static int romanToInt(String s) {
+        int sum = 0;
+        Map<Character, Integer> map = new HashMap<>();
+        map.put('I', 1);
+        map.put('V', 5);
+        map.put('X', 10);
+        map.put('L', 50);
+        map.put('C', 100);
+        map.put('D', 500);
+        map.put('M', 1000);
+        for (int i = 0; i < (s.length() - 1); i ++) {
+            if (map.get(s.charAt(i)) < map.get(s.charAt(i + 1))) {
+                sum -= map.get(s.charAt(i));
+            } else {
+                sum += map.get(s.charAt(i));
+            }
+        }
+//        System.out.println(sum += map.get(s.charAt(s.length() - 1)));
+        return sum += map.get(s.charAt(s.length() - 1));
+    }
+    public static void main(String[] args) {
+        romanToInt("MCMXCIV");
+    }
+}
